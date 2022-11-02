833c21af20db10e39b3ec59ebdc8b324c44c8cac
John Kang <johnkang1121@gmail.com>
2022-11-02 00:59:18 -0700

    made changes to js, css, and html

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// Show the curremt date and current time
+$(document).ready(function() {
+
+    var today = moment();
+    
+    $("#currentDay").text(today.format("[Current Date:] dddd MMM Do, YYYY"));
+    $("#currentTime").text(today.format("[Current Time:] h:mm a [PST]"));
+});
